cfca3fd4917306bdf1e285063a64dbae0ce4dabd
burak.atay <burak.atay@ininal.com>
2021-05-19 03:49:58 +0300

    Exception message is fixed

--- src/main/java/com/ataybur/garage/exception/GarageHasNoProperSlotsException.java
+++ src/main/java/com/ataybur/garage/exception/GarageHasNoProperSlotsException.java
@@ -2,6 +2,6 @@ package com.ataybur.garage.exception;
 
 public class GarageHasNoProperSlotsException extends RuntimeException {
     public GarageHasNoProperSlotsException() {
-        super("Garage has no proper Slots");
+        super("Garage has no proper slots");
     }
 }
